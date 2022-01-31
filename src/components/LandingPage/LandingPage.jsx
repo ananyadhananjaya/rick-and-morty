684b7b54fa5fbc3348e61db169c4f2bf59df0a48
ananyadhananjaya <ananya.dhananjaya1998@gmail.com>
2022-01-31 23:58:47 +0530

    Initial setup with Material UI

---
+++ src/components/LandingPage/LandingPage.jsx
@@ -0,0 +1,9 @@
+import { Grid } from "@mui/material"
+
+export const LandingPage = () =>{
+    return(
+        <Grid container justifyContent={"center"}>
+            <Grid item>Rick and morty - initial</Grid>
+        </Grid>
+    )
+}
